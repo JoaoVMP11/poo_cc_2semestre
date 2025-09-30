a19be360e5bd07eab12b8712f7a8ac4756a2bb67
Joao Vitor <jvmp1176@gmail.com>
2025-09-30 16:49:37 -0300

    att exercicio3 lista

--- exercicioslistajava/exercicio3/src/exercicio3/Principal.java
+++ exercicioslistajava/exercicio3/src/exercicio3/Principal.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Principal {
 	
-	public static ArrayList<Integer> exclusivoDuasListas (ArrayList<Integer> lista1, ArrayList<Integer> lista2){
-		ArrayList<Integer> exclusivo= new ArrayList<Integer>();
+	public static ArrayList<Integer> comumDuasListas (ArrayList<Integer> lista1, ArrayList<Integer> lista2){
+		ArrayList<Integer> interseccao= new ArrayList<Integer>();
 		for (int i=0; i<lista1.size(); i++) {
 			for (int j=0; j<lista2.size();j++) {
-				if (!(lista1.get(i)==lista2.get(j))) {
-					exclusivo.add(lista1.get(i));
+				if (lista1.get(i)==lista2.get(j)) {
+					interseccao.add(lista1.get(i));
 				}
 			}
 			
 		}
 		
 		
-		return exclusivo;
+		return interseccao;
 		
 	}
 
@@ -40,6 +40,6 @@ public class Principal {
 	primos.add(13);
 	primos.add(17);
 	
-	System.out.println(exclusivoDuasListas(gratio, primos));
+	System.out.println(comumDuasListas(gratio, primos));
 	}
 }
